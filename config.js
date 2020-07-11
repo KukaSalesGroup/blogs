3686a82f4539fde18e7f6870be9ce2ae603cb9d8
Shawn McCrum <xmccrum@gmail.com>
2020-07-11 14:42:37 -0600

    Styled blog page

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   url: 'https://lumen.netlify.com',
   pathPrefix: '/',
   title: 'Blog by Kuka Sales',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  subtitle: 'Kuka Sales Group specializes in meeting all your materials testing needs',
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
@@ -23,14 +23,14 @@ module.exports = {
   author: {
     name: 'Kuka Sales',
     photo: '/photo.png',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    bio: 'Kuka Sales Group specializes in meeting all your materials testing needs',
     contacts: {
       email: 'sales@kukasales.com',
       facebook: 'official.kukasales',
       twitter: 'kukasalesgroup',
       linkedin: 'company/kuka-sales-group',
       instagram: 'kukasalesgroup',
-      youtube: '#',
+      youtube: 'UC-sHw4og1QSzYjuqaJg1ECw',
     }
   }
 };
